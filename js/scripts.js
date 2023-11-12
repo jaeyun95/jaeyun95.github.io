1b2a2523b4eafc6514de0713636ecfd96180e423
jaeyun95 <jaeyoon_95@naver.com>
2023-11-13 00:29:40 +0900

    plz...

--- js/scripts.js
+++ js/scripts.js
@@ -139,7 +139,7 @@ function getKakaoMap2() {
 
     const map = new kakao.maps.Map(mapContainer, mapOption);
 
-    var imageSrc = 'assets/img/map_point.png', // 마커이미지의 주소입니다    
+    var imageSrc = '../assets/img/map_point.png', // 마커이미지의 주소입니다    
         imageSize = new kakao.maps.Size(64, 69), // 마커이미지의 크기입니다
         imageOption = {offset: new kakao.maps.Point(27, 69)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
 
