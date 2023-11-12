974bbf33dee4e8f741135ad6117622dd6c16c88b
jaeyun95 <jaeyoon_95@naver.com>
2023-11-12 21:53:31 +0900

    add d-day

--- js/scripts.js
+++ js/scripts.js
@@ -78,3 +78,40 @@ function fadeIn(el, display) {
         }
     })();
 };
+
+
+// D-Day 설정 (년, 월(0부터 시작), 일)
+const dDay = new Date('2023-12-31');
+
+// D-Day까지의 시간 계산하는 함수
+function calculateDday() {
+    const now = new Date();
+    const difference = dDay.getTime() - now.getTime();
+
+    // 시간 차이를 일 단위로 변환
+    const daysLeft = Math.ceil(difference / (1000 * 60 * 60 * 24));
+
+    return daysLeft;
+}
+
+// D-N Day를 HTML에 표시하는 함수
+function displayDday() {
+    const daysLeft = calculateDday();
+    const countdownElement = document.getElementById('countdown');
+
+    if (daysLeft === 0) {
+        countdownElement.innerHTML = "D-Day";
+    } else if (daysLeft > 0) {
+        countdownElement.innerHTML = `D-${daysLeft} Day`;
+    } else {
+        countdownElement.innerHTML = "D+${daysLeft} Day";
+    }
+}
+
+// 페이지가 로드될 때 D-N Day 표시
+window.onload = function() {
+    displayDday();
+
+    // 매 초마다 갱신되도록 설정
+    setInterval(displayDday, 1000);
+};
