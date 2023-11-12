9a5672bb9e68e809502c189a1e514c3457cdeb9e
jaeyun95 <jaeyoon_95@naver.com>
2023-11-12 23:13:59 +0900

    map test

--- js/scripts.js
+++ js/scripts.js
@@ -129,22 +129,30 @@ function getKakaoMap() {
 }
 
 function getKakaoMap2() {
-    const mapContainer = document.getElementById('map');
-    const mapOption = {
-        center: new kakao.maps.LatLng(37.52052, 127.022581), // '빌라드지디 청담'의 위도, 경도
-        level: 3 // 지도 확대 레벨
+    var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
+    mapOption = { 
+        center: new kakao.maps.LatLng(37.54699, 127.09598), // 지도의 중심좌표
+        level: 4 // 지도의 확대 레벨
     };
 
-    const map = new kakao.maps.Map(mapContainer, mapOption);
-    const markerPosition = new kakao.maps.LatLng(37.52052, 127.022581);
+    var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
 
-    // 마커 표시
-    const marker = new kakao.maps.Marker({
-        position: markerPosition
+    var imageSrc = 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png', // 마커이미지의 주소입니다    
+        imageSize = new kakao.maps.Size(64, 69), // 마커이미지의 크기입니다
+        imageOption = {offset: new kakao.maps.Point(27, 69)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+        
+    // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
+    var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption),
+        markerPosition = new kakao.maps.LatLng(37.54699, 127.09598); // 마커가 표시될 위치입니다
+
+    // 마커를 생성합니다
+    var marker = new kakao.maps.Marker({
+        position: markerPosition, 
+        image: markerImage // 마커이미지 설정 
     });
 
-    // 마커 지도에 추가
-    marker.setMap(map);
+    // 마커가 지도 위에 표시되도록 설정합니다
+    marker.setMap(map);  
 }
 
 // 페이지가 로드될 때 D-N Day 표시
@@ -153,6 +161,6 @@ window.onload = function() {
 
     // 매 초마다 갱신되도록 설정
     setInterval(displayDday, 1000);
-    getKakaoMap();
+    getKakaoMap2();
     //getKakaoMap();
 };
