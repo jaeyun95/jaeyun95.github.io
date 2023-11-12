31efd2b91eef935234df0c0767c3c9072b78c440
jaeyun95 <jaeyoon_95@naver.com>
2023-11-13 00:28:44 +0900

    plz...

--- js/scripts.js
+++ js/scripts.js
@@ -134,19 +134,54 @@ function getKakaoMap2() {
     const mapContainer = document.getElementById('map');
     const mapOption = {
         center: new kakao.maps.LatLng(37.52020441860761, 127.05544016257608), // '빌라드지디 청담'의 위도, 경도
-        level: 3 // 지도 확대 레벨
+        level: 4 // 지도 확대 레벨
     };
 
     const map = new kakao.maps.Map(mapContainer, mapOption);
-    const markerPosition = new kakao.maps.LatLng(37.52020441860761, 127.05544016257608);
+
+    var imageSrc = 'assets/img/map_point.png', // 마커이미지의 주소입니다    
+        imageSize = new kakao.maps.Size(64, 69), // 마커이미지의 크기입니다
+        imageOption = {offset: new kakao.maps.Point(27, 69)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+
+    // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
+    var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption),
+        markerPosition = new kakao.maps.LatLng(37.52020441860761, 127.05544016257608); // 마커가 표시될 위치입니다
+
+    // 마커를 생성합니다
+    var marker = new kakao.maps.Marker({
+    position: markerPosition,
+    image: markerImage // 마커이미지 설정 
+    });
+
+
+
+
+    // const markerPosition = new kakao.maps.LatLng(37.52020441860761, 127.05544016257608);
 
     // 마커 표시
-    const marker = new kakao.maps.Marker({
-        position: markerPosition
-    });
+    // const marker = new kakao.maps.Marker({
+    //     position: markerPosition
+    // });
 
     // 마커 지도에 추가
     marker.setMap(map);
+
+     // 커스텀 오버레이에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+     var content = '<div class="customoverlay">' +
+     '  <a href="https://place.map.kakao.com/1417946142" target="_blank">' +
+     '    <span class="title">빌라드지디 청담</span>' +
+     '  </a>' +
+     '</div>';
+
+     // 커스텀 오버레이가 표시될 위치입니다 
+    var position = new kakao.maps.LatLng(37.52020441860761, 127.05544016257608);  
+
+    // 커스텀 오버레이를 생성합니다
+    var customOverlay = new kakao.maps.CustomOverlay({
+        map: map,
+        position: position,
+        content: content,
+        yAnchor: 1 });
 }
 
 // 페이지가 로드될 때 D-N Day 표시
